bff4af1ddbd032c6bdc7083b3271f6f4a3e8f38e
xusanoff <xusanoff.abdurahmon@gmail.com>
2026-05-05 20:08:54 +0500

    Update student.py

--- models/student.py
+++ models/student.py
@@ -12,16 +12,8 @@ class Student(db.Model):
     full_name    = db.Column(db.String(120), nullable=False)
     phone_number = db.Column(db.String(20), unique=True)
     comment      = db.Column(db.Text)
-    created_by = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="SET NULL"))
+    created_by   = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="SET NULL"))
     created_at   = db.Column(db.DateTime, default=lambda: datetime.now(time_zone))
-
-
-    groups = db.relationship("Group", cascade="all, delete-orphan", passive_deletes=True)
-    enrollments = db.relationship("Enrollment",back_populates="student",cascade="all, delete-orphan",passive_deletes=True)
-    payments = db.relationship("Payment", cascade="all, delete-orphan", passive_deletes=True)
-    debts = db.relationship("Debt", cascade="all, delete-orphan", passive_deletes=True)
-    monthly_debts = db.relationship("MonthlyDebt", cascade="all, delete-orphan", passive_deletes=True)
-    attendance = db.relationship("Attendance", cascade="all, delete-orphan", passive_deletes=True)
 
     def __init__(self, full_name, phone_number=None, comment=None, created_by=None):
         super().__init__()
